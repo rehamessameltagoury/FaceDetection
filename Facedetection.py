bc20ae1031f5bce162bfd344a376e6c8dbdc2d1b
rehamessameltagoury <36308055+rehamessameltagoury@users.noreply.github.com>
2020-05-05 01:34:04 +0200

    Add files via upload

---
+++ Facedetection.py
@@ -0,0 +1,38 @@
+import os.path
+import numpy as np
+import cv2
+import json
+from flask import Flask,request,Response
+import uuid
+
+
+def faceDetect(img):
+    face_cascade = cv2.CascadeClassifier('face.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    #print(faces)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0))
+    # save the file
+    path_file =('static/%s.jpg' %uuid.uuid4().hex)
+    cv2.imwrite(path_file,img)
+    return json.dumps(path_file)
+
+
+# API
+
+app = Flask(__name__)
+
+@app.route('/api/upload',methods=['POST'])
+
+
+def upload():
+    img = cv2.imdecode(np.fromstring(request.files['image'].read(),np.uint8),cv2.IMREAD_UNCHANGED)
+    # process image
+    img_processed =faceDetect(img)
+    # response
+    return Response(response=img_processed,status=200,mimetype="application/json")
+
+
+app.run(host="0.0.0.0",port=5000)
+
